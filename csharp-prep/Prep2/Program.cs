17910721e903cc9fc387d06259f6459d3201e3ea
Adão Klepa Junior <klepa.geral@gmail.com>
2024-09-20 09:27:28 -0300

    Learning Activity - C# Prep 2 Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is you grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80 && number < 90)
+        {
+            letter = "B";
+        }
+        else if (number >= 70 && number < 80)
+        {
+            letter = "C";
+        }
+        else if (number >= 60 && number < 70)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Wish you a better luck next time!");
+        }
+
+
+
     }
 }
